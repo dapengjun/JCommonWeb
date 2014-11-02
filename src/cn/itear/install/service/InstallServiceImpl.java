37ca70b29eff3269ddadabf58c50feba14536958
dapengjun <357969888@163.com>
2014-11-02 17:55:48 +0800

    fix warnings

--- src/cn/itear/install/service/InstallServiceImpl.java
+++ src/cn/itear/install/service/InstallServiceImpl.java
@@ -1,18 +1,11 @@
 package cn.itear.install.service;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import cn.itear.common.service.BaseServiceImpl;
 import cn.itear.install.dao.IInstallDao;
-import cn.itear.perm.dao.IUserDao;
-import cn.itear.perm.po.UserPo;
 
 @Transactional
 @Service("installService")
